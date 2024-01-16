d8b39cd0bcff0f9939dede2c06bee5510f1441e8
Lois Lanctot <lanctot.lois@student.greenriver.edu>
2024-01-16 12:55:59 -0500

    added controller file

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
+/*
+ * Lois Lanctot
+ * January 2024
+ * https://lois.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
 
-echo "Diner";
+//Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once ('vendor/autoload.php');
+
+//Instantiate F3
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo "My Diner";
+});
+
+//Run Fat-Free
+$f3->run();
+
+
